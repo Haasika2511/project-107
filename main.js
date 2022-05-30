1356ed457d187b478ad783f5230a19a231f47483
Haasika2511 <83579175+Haasika2511@users.noreply.github.com>
2022-05-30 11:56:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+navigator.mediaDevices.getUserMedia({ audio: true });
+classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/DdsZVR6AM/model.json", modelReady);
+
+
+function modelReady(){
+classifier.classify(gotResults);
+}
+
+function gotResults(error,results){
+console.log("got the result");
+}
